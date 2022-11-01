c98fcdd8e9c18b8045ea02215662165a3f26437e
Dheeraj kumar <Howtotech000@gmail.com>
2022-11-01 19:57:04 +0530

    bug fix

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <>
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        <a href="."><img src={images.logo} alt="logo" /></a>
       </div>
       <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact', 'resume'].map((item) => (
