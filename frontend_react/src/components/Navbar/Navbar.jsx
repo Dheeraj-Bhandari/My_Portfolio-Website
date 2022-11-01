8f96cf56aa51250635db4d4770748088cc9e1959
Dheeraj kumar <Howtotech000@gmail.com>
2022-11-01 19:03:12 +0530

    bug fix

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,71 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
-import './Navbar.scss'
-import { images } from '../../constants'
 
-
+import { images } from '../../constants';
+import './Navbar.scss';
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <>
-      <nav className='app__navbar'>
-        <div className='app__navabr-logo'>
-          <img  src={images.logo} alt="logo" />
-        </div>
-        <ul className='app__navabr-links'>
-          {['home', 'about', 'work', 'skills', 'contact', 'resume'].map((item) => (
-            <li className='app__flex p-text' key={`link-${item}`}>
-
-              <div />
-              {item === 'resume' ?
-                <a href='https://docs.google.com/document/d/1UfCPCepAa2XXJ2z7HxYfP9zzkRrFdmS4/edit?usp=sharing&ouid=109558565411947281000&rtpof=true&sd=true' target={'_blank'} rel="noreferrer">{item}</a> :
-
-                <a href={`#${item}`} rel="noreferrer">{item}</a>
-
-              }
-
-
-            </li>
-          ))}
-        </ul>
-
-        <div className='app__navbar-menu'>
-          <HiMenuAlt4 onClick={() => setToggle(true)} />
-          {toggle && (
-            <motion.div
-              whileInView={{ x: [300, 0] }}
-              transition={{ duration: 0.85, ease: 'easeOut' }}
-            >
-              <HiX onClick={() => setToggle(false)} />
-              <ul>
-                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                  <li className='app__flex p-text' key={item}>
-
-
-                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-
-
-                  </li>
-                ))}
-              </ul>
-            </motion.div>
-          )}
-        </div>
-      </nav>
-      <div className="mycopyright">
-        <p className="p-text"></p>
-        <a href="https://docs.google.com/document/d/1UfCPCepAa2XXJ2z7HxYfP9zzkRrFdmS4/edit?usp=sharing&ouid=109558565411947281000&rtpof=true&sd=true"
-          target={'_blank'}
-          rel="noreferrer"
-        >
-          <img src="https://cdn-icons-png.flaticon.com/512/6614/6614677.png" alt="resume icon" />
-        </a>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
       </div>
+      <ul className="app__navbar-links">
+        {['home', 'about', 'work', 'skills', 'contact', 'resume'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            {item === 'resume' ?
+              <a href='https://docs.google.com/document/d/1UfCPCepAa2XXJ2z7HxYfP9zzkRrFdmS4/edit?usp=sharing&ouid=109558565411947281000&rtpof=true&sd=true' target={'_blank'} rel="noreferrer">{item}</a> :
 
-    </>
+              <a href={`#${item}`} rel="noreferrer">{item}</a>
+            }
+          </li>
+        ))}
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact', 'resume'].map((item) => (
+                <li key={item}>
+                  {/* <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a> */}
+                  {item === 'resume' ?
+                    <a onClick={() => setToggle(false)} href='https://docs.google.com/document/d/1UfCPCepAa2XXJ2z7HxYfP9zzkRrFdmS4/edit?usp=sharing&ouid=109558565411947281000&rtpof=true&sd=true' target={'_blank'} rel="noreferrer">{item}</a> :
+
+                    <a onClick={() => setToggle(false)} href={`#${item}`} rel="noreferrer">{item}</a>
+                  }
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
   );
 };
 
-export default Navbar
+export default Navbar;
